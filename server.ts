d555a36119fd68ae0e885823b9a956d8f63db99d
EVGENY KVEST <evgenykvest@gmail.com>
2026-06-24 09:44:42 +0200

    WS: Healthcheck + Heartbeat config

--- server.ts
+++ server.ts
@@ -3,14 +3,30 @@ import {createServer} from 'http'
 import {Server} from 'socket.io'
 
 const dev = process.env.NODE_ENV !== 'production'
+
 const app = next({dev})
+
+// Handler for request / response handling of nextjs
 const handler = app.getRequestHandler()
 
-const httpServer = createServer(handler) 
+// Create a HTTP Server
+const httpServer = createServer((req, res) => {
+    if(req.url === '/health') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({status: 'ok'}))
+        return
+    }
+    handler(req, res)   
+}) 
 
-const io = new Server(httpServer)
+// Create a Socket.IO Server
+// PingPong: PingInterval: 25000ms, PingTimeout: 20000ms - Heartbeat to detect disconnection (explicit for better understanding)
+const io = new Server(httpServer, {pingInterval: 25000, pingTimeout: 20000})
 
+// Prepare the app for handling requests
 app.prepare().then(() => {
+    // Handle socket connections
     io.on('connection', (socket) => {
         console.log(`Socket Connetected: ${socket.id}`)
         socket.on('message:send', (text) => {
@@ -25,5 +41,7 @@ app.prepare().then(() => {
     httpServer.listen("3000")
     console.log('Server is listening on port 3000')
 })
+
+
 
 
